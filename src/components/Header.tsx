c3edfd91d0ab2a892b73fe718984367c63ef97fd
Steve Stafford <76266357+stevens69400@users.noreply.github.com>
2025-05-10 22:33:38 +0000

    Ready for Production

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header: React.FC = () => {
             <Container className="!px-0">
                 <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-0">
                     {/* Logo */}
-                    <Link href="/" className="flex items-center gap-x-4 w-full justify-center md:justify-start md:justify-center md:w-auto">
+                    <Link href="/" className="flex items-center gap-x-4 w-full justify-center md:justify-start md:w-auto">
                         <Image
                             src="/images/logo.png"
                             alt="Logo"
