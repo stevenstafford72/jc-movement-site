1de6d4fe6a349b9877c9e967337a0444ba9aee1a
Steve Stafford <76266357+stevenstafford72@users.noreply.github.com>
2026-03-12 17:02:53 +0000

    Update Next.js to secure version 16.0.10

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
-    heading: 'It’s Bigger Than Us', // Corrected or added heading
+    heading: 'It’s Bigger Than Us', // Corrected or added headings
     subheading: 'Your generous donations have helped J.C. Movement, INC. deliver more than 10,013 meals to our kids  & families since 2017. Thank You',
     centerImageSrc: '/images/hero-mockup.webp',
 }
